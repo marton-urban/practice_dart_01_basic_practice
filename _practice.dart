c6295ad94d75625d2149be714090226799a661dd
urban.marton <urban.marton@gmail.com>
2021-11-10 17:19:55 +0100

    add null safety material

--- _practice.dart
+++ _practice.dart
@@ -1,22 +1,42 @@
-extension IterableNumX<T extends num> on Iterable {
-  T? sum() {
-    var sum;
-    for (var current in this) {
-      if (current != null) {
-        if (sum == null) {
-          sum = current;
-        } else {
-          sum += current;
-        }
-      }
-    }
-    return sum;
+// function
+void foo() => print('foo');
+
+// function
+String bar() {
+  return 'bar';
+}
+
+void fooBar() {
+  int add(int a, int b) => a + b; // inline function
+
+  int value = 0;
+  for (var i = 0; i < 9; i++) {
+    value = add(value, i); // call of inline function
+    print(value);
+  }
+}
+
+class SomeClass {
+  static void foo() => print(
+      'foo'); // function in class context sometimes called static method but actually not a method
+
+  SomeClass(this.firstName);
+
+  String firstName;
+
+  // a real method with implicit access to `this`
+  void bar() {
+    print('$firstName bar');
+    print('$firstName bar'); // this can and should be omitted in Dart
+
+    void doSomething() =>
+        print('doSomething'); // inline function declared in a method
+
+    doSomething(); // call of inline function
   }
 }
 
 void main() {
-  final total = ['cool'].sum();
-  final total2 = [null, null, 3.5].sum();
-  print(total);
-  print(total2);
+  var aba = SomeClass('Marci');
+  aba.bar();
 }
