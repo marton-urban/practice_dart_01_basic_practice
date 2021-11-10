f615f5a44d60ac5586f5bd3055eea7a0e7774fb4
urban.marton <urban.marton@gmail.com>
2021-11-11 00:45:56 +0100

    upload new files

---
+++ futures3.dart
@@ -0,0 +1,26 @@
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String task2Result = await task2();
+  task3(task2Result);
+}
+
+void task1() {
+  print('Task 1 complete');
+}
+
+Future<String> task2() async {
+  String result = 'task 2 data';
+  await Future.delayed(Duration(seconds: 3), () {
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+  return result;
+}
+
+void task3(String task2Data) {
+  print('Task 3 complete with $task2Data');
+}
