a7fde4de190afd7f3aa3da059e19780cf4f5a2c1
Marton Urban <urban.marton@gmail.com>
2022-12-05 08:26:02 +0100

    update

--- _practice2.dart
+++ _practice2.dart
@@ -1,1 +1,16 @@
+class Temperature {
+  Temperature.celsius(this.celsius);
+  Temperature.farenheit(double farenheit) : celsius = (farenheit - 32) / 1.8;
+  double celsius;
+  double get farenheit => celsius * 1.8 + 32;
+  set farenheit(double farenheit) => celsius = (farenheit - 32) / 1.8;
+}
 
+void main() {
+  final temp1 = Temperature.celsius(30);
+  final temp2 = Temperature.farenheit(90);
+  print(temp1.celsius);
+  print(temp1.farenheit);
+  print(temp2.celsius);
+  print(temp2.farenheit);
+}
