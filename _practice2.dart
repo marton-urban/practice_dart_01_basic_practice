4f595a1d2e1b1622e39afce22289ea30e0791f1b
Márton Urbán <urban.marton@gmail.com>
2022-12-09 02:07:28 +0100

    update

--- _practice2.dart
+++ _practice2.dart
@@ -1,20 +1,21 @@
-// this solution also works for [] and [ x ] as well
-import 'package:collection/collection.dart';
-
-int? firstNonConsecutive(List<int> arr) {
-  final missing = arr.isEmpty
-      ? null // add check so it also works for []
-      : List.generate(
-              arr.last - arr.first + 1,
-              (index) =>
-                  arr.first +
-                  index) // generate consecutive list without missing element
-          .firstWhereOrNull((e) => !arr.contains(
-              e)); // find first missing element by comparing to generated list
-  return missing != null ? missing + 1 : null;
-}
+import "package:test/test.dart";
+import "package:solution/solution.dart";
+import "dart:math";
 
 void main() {
-  print(firstNonConsecutive(const []));
-  print('cool');
+  group("fixed tests", () {
+    test("Testing for [1, 2, 3, 4, 5]", () => expect(positiveSum([1, 2, 3, 4, 5]), equals(15)));
+    test("Testing for [1, -2, 3, 4, 5]", () => expect(positiveSum([1, -2, 3, 4, 5]), equals(13)));
+    test("Testing for []", () => expect(positiveSum([]), equals(0)));
+    test("Testing for [-1, -2, -3, -4, -5]", () => expect(positiveSum([-1, -2, -3, -4, -5]), equals(0)));
+    test("Testing for [-1, 2, 3, 4, -5]", () => expect(positiveSum([-1, 2, 3, 4, -5]), equals(9)));
+  });
+  group("Random tests", () {
+    Random rinst = Random();
+    int sol(List<int> arr) => arr.where((x) => x > 0).fold(0, (a, b) => a + b);
+    for (int i = 0; i < 100; i++) {
+      List<int> arr = List<int>.generate(rinst.nextInt(20) + 1, (_) => rinst.nextInt(200) - 100);
+      test("Testing for $arr", () => expect(positiveSum([]..addAll(arr)), equals(sol(arr))));
+    }
+  });
 }
