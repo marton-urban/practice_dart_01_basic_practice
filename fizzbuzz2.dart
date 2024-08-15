34984453155dc17dfc1d704cc1b136f53591a87c
Marton Urban <urban.marton@gmail.com>
2024-08-15 22:32:35 +0200

    use generate instead of for

--- fizzbuzz2.dart
+++ fizzbuzz2.dart
@@ -18,5 +18,6 @@ void main() {
 
 extension RangeExtension on int {
   List<int> to(int maxInclusive, {int step = 1}) =>
-      [for (int i = this; i <= maxInclusive; i += step) i];
+      // [for (int i = this; i <= maxInclusive; i += step) i];
+      List.generate(maxInclusive, (i) => i + step);
 }
